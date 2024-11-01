db2976dee5991497b7fc928ab3da2265299c44af
HazemAhmed9575 <hAZEM.AHMED9575@GMAIL.COM>
2024-11-01 14:20:23 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,132 @@
+// =========================================================
+let TexFrom = document.querySelector("#TexFrom");
+let TexTo = document.querySelector("#TexTo");
+let TranslatFrom = document.querySelector("#TranslatFrom");
+let TranslatTo = document.querySelector("#TranslatTo");
+let Translat = document.querySelector("#Translat");
+// ==========================================================
+const data =   {
+  "am-ET": "Amharic",
+  "ar-SA": "Arabic",
+  "be-BY": "Bielarus",
+  "bem-ZM": "Bemba",
+  "bi-VU": "Bislama",
+  "bjs-BB": "Bajan",
+  "bn-IN": "Bengali",
+  "bo-CN": "Tibetan",
+  "br-FR": "Breton",
+  "bs-BA": "Bosnian",
+  "ca-ES": "Catalan",
+  "cop-EG": "Coptic",
+  "cs-CZ": "Czech",
+  "cy-GB": "Welsh",
+  "da-DK": "Danish",
+  "dz-BT": "Dzongkha",
+  "de-DE": "German",
+  "dv-MV": "Maldivian",
+  "el-GR": "Greek",
+  "en-GB": "English",
+  "es-ES": "Spanish",
+  "et-EE": "Estonian",
+  "eu-ES": "Basque",
+  "fa-IR": "Persian",
+  "fi-FI": "Finnish",
+  "fn-FNG": "Fanagalo",
+  "fo-FO": "Faroese",
+  "fr-FR": "French",
+  "gl-ES": "Galician",
+  "gu-IN": "Gujarati",
+  "ha-NE": "Hausa",
+  "he-IL": "Hebrew",
+  "hi-IN": "Hindi",
+  "hr-HR": "Croatian",
+  "hu-HU": "Hungarian",
+  "id-ID": "Indonesian",
+  "is-IS": "Icelandic",
+  "it-IT": "Italian",
+  "ja-JP": "Japanese",
+  "kk-KZ": "Kazakh",
+  "km-KM": "Khmer",
+  "kn-IN": "Kannada",
+  "ko-KR": "Korean",
+  "ku-TR": "Kurdish",
+  "ky-KG": "Kyrgyz",
+  "la-VA": "Latin",
+  "lo-LA": "Lao",
+  "lv-LV": "Latvian",
+  "men-SL": "Mende",
+  "mg-MG": "Malagasy",
+  "mi-NZ": "Maori",
+  "ms-MY": "Malay",
+  "mt-MT": "Maltese",
+  "my-MM": "Burmese",
+  "ne-NP": "Nepali",
+  "niu-NU": "Niuean",
+  "nl-NL": "Dutch",
+  "no-NO": "Norwegian",
+  "ny-MW": "Nyanja",
+  "ur-PK": "Pakistani",
+  "pau-PW": "Palauan",
+  "pa-IN": "Panjabi",
+  "ps-PK": "Pashto",
+  "pis-SB": "Pijin",
+  "pl-PL": "Polish",
+  "pt-PT": "Portuguese",
+  "rn-BI": "Kirundi",
+  "ro-RO": "Romanian",
+  "ru-RU": "Russian",
+  "sg-CF": "Sango",
+  "si-LK": "Sinhala",
+  "sk-SK": "Slovak",
+  "sm-WS": "Samoan",
+  "sn-ZW": "Shona",
+  "so-SO": "Somali",
+  "sq-AL": "Albanian",
+  "sr-RS": "Serbian",
+  "sv-SE": "Swedish",
+  "sw-SZ": "Swahili",
+  "ta-LK": "Tamil",
+  "te-IN": "Telugu",
+  "tet-TL": "Tetum",
+  "tg-TJ": "Tajik",
+  "th-TH": "Thai",
+  "ti-TI": "Tigrinya",
+  "tk-TM": "Turkmen",
+  "tl-PH": "Tagalog",
+  "tn-BW": "Tswana",
+  "to-TO": "Tongan",
+  "tr-TR": "Turkish",
+  "uk-UA": "Ukrainian",
+  "uz-UZ": "Uzbek",
+  "vi-VN": "Vietnamese",
+  "wo-SN": "Wolof",
+  "xh-ZA": "Xhosa",
+  "yi-YD": "Yiddish",
+  "zu-ZA": "Zulu"
+}
+    Object.entries(data).map((ele) => {
+      ele[1] == "English"
+        ? (TranslatFrom.innerHTML += `<option  value="${ele[0]}" selected >${ele[1]}</option>`)
+        : (TranslatFrom.innerHTML += `<option  value="${ele[0]}" >${ele[1]}</option>`);
+      ele[1] == "Arabic"
+        ? (TranslatTo.innerHTML += `<option  value="${ele[0]}"  selected >${ele[1]}</option>`)
+        : (TranslatTo.innerHTML += `<option  value="${ele[0]}" >${ele[1]}</option>`);
+    });
+
+
+Translat.addEventListener("click", async () => {
+  if (TexFrom.value == "" || TranslatFrom.value == TranslatTo.value) {
+    TexTo.innerHTML =
+      " Please make sure you write a sentence and do not choose the same language as the translation";
+  } else {
+    try {
+      let res = await fetch(
+        `https://api.mymemory.translated.net/get?q=${TexFrom.value}&langpair=${TranslatFrom.value}|${TranslatTo.value}`
+      );
+      let data = await res.json();
+      TexTo.innerHTML = data.matches[1].translation;
+    } catch (e) {
+      TexTo.innerHTML = `Eror Api:${e.message}`;
+    }
+  }
+});
